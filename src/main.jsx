81a8e3e20a3a8a015aa193aec60e5a3bc202ff20
Maryam71dashti <maryam.a.dashti71@gmail.com>
2025-10-02 01:19:50 +0330

    افزودن کامپوننت‌های جدید و تغییرات در استایل

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,18 @@
-
-import { ReactDOM } from 'react-dom/client'
+import React from 'react'
+import  ReactDOM  from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
+import ShopContextProvider from './context/ShopContext.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <React.StrictMode>
+    <BrowserRouter>
+    <ShopContextProvider>
+      <App/>
+    </ShopContextProvider>
+    
+    </BrowserRouter>
+  </React.StrictMode>
 )
